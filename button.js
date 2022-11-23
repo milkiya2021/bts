4f061bff3c455ab843c682b0acfe7d751f71796f
Hebom <87565110+milkiya2021@users.noreply.github.com>
2022-11-23 22:40:51 +0900

    Add files via upload
    - 버튼 눌러 스크롤 되기 
    - 멤버이미지 교체

---
+++ button.js
@@ -0,0 +1,30 @@
+//메뉴버튼 누를 시 해당위치로 스크롤 이동하는 연출보여주기  ㅋㅋㅋ
+
+const button = document.querySelectorAll("button");
+const sectionbox = document.querySelectorAll(".section_box");
+
+const firstTop = sectionbox[0].offsetTop;
+const secondTop = sectionbox[1].offsetTop;
+const thirdTop = sectionbox[2].offsetTop;
+const fourthTop = sectionbox[3].offsetTop;
+const fifthTop = sectionbox[4].offsetTop;
+
+button[0].onclick = function () {
+  window.scroll({ top: firstTop, behavior: "smooth" });
+};
+
+button[1].onclick = function () {
+  window.scroll({ top: secondTop, behavior: "smooth" });
+};
+
+button[2].onclick = function () {
+  window.scroll({ top: thirdTop, behavior: "smooth" });
+};
+
+button[3].onclick = function () {
+  window.scroll({ top: fourthTop, behavior: "smooth" });
+};
+
+button[4].onclick = function () {
+  window.scroll({ top: fifthTop, behavior: "smooth" });
+};
